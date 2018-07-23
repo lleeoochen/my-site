3925b13e1b41789ce5ffd7f4f50946295904f315
Wei Tung Chen <lleeoochen@gmail.com>
2018-07-22 20:06:05 -0700

    improved header

--- src/views/components/Header/index.js
+++ src/views/components/Header/index.js
@@ -22,18 +22,20 @@ export default class Header extends Component {
 	render() {
 		return (
 			<Menu
-				className={"menu"}
 				fixed={"top"}
 				color={"black"}
 				inverted={true}
 				pointing={true}
 				borderless={true}>
 				<Container>
-					<Menu.Item as={ NavLink } to='/home'>	Home </Menu.Item>
-					<Menu.Item as={ NavLink } to='/coding'> Coding </Menu.Item>
-					<Menu.Item as={ NavLink } to='/music'> Music </Menu.Item>
-					<Menu.Item as={ NavLink } to='/picture'> Picture </Menu.Item>
-					<Menu.Item position='right'> About </Menu.Item>
+					<Menu.Item className={'home'} as={ NavLink } to='/home'>HOME</Menu.Item>
+					<Menu.Item as={ NavLink } to='/coding'>Coding Projects</Menu.Item>
+					<Menu.Item as={ NavLink } to='/music'>Music Recordings</Menu.Item>
+					<Menu.Item as={ NavLink } to='/picture'>Photography</Menu.Item>
+					<Menu.Item as={ NavLink } to='/picture'>Educational Videos</Menu.Item>
+					{
+						//<Menu.Item className={'about'} as={ NavLink } to='/picture'>About</Menu.Item>
+					}
 				</Container>
 			</Menu>
 		);
