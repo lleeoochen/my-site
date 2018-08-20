59d8ce0fc46c774e45217e20dbd0c06fd57b94e1
Wei Tung Chen <lleeoochen@gmail.com>
2018-08-19 18:53:07 -0700

    Modularized header component with major improvement

--- src/views/components/Header/index.js
+++ src/views/components/Header/index.js
@@ -1,56 +1,95 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { Toolbar, Tab } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Button, Menu, MenuItem } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import Constants from 'helpers/Constants';
 import './index.css';
 
 export default class Header extends Component {
 
+	state = {
+		menuOpen: false
+	}
+
+	openMenu(open) {
+		this.setState({
+			menuOpen: open
+		});
+	}
+
 	render() {
 		let styles = createStyles();
 
+		let dataNav = [
+			{
+				title: Constants.PAGES.HOME.TITLE,
+				route: Constants.PAGES.HOME.ROUTE
+			},
+			{
+				title: Constants.PAGES.CODING.TITLE,
+				route: Constants.PAGES.CODING.ROUTE
+			},
+			{
+				title: Constants.PAGES.MUSIC.TITLE,
+				route: Constants.PAGES.MUSIC.ROUTE
+			},
+			{
+				title: Constants.PAGES.PICTURE.TITLE,
+				route: Constants.PAGES.PICTURE.ROUTE
+			}
+		];
+
+		let topHeaders = dataNav.map((data, index) =>
+			<Button
+				key={index}
+				style={styles.menuItem}
+				component={NavLink}
+				to={data.route}>
+				{data.title}
+			</Button>
+		);
+
+		let menuHeaders = dataNav.map((data, index) =>
+			<MenuItem
+				key={index}
+				style={styles.menuItem}
+				component={NavLink}
+				onClick={() => this.openMenu(false)}
+				to={data.route}>
+				{data.title}
+			</MenuItem>
+		);
+
 		return (
-			<Toolbar style={styles.menu}>
-				<Tab
-					style={{...styles.menuItem, ...styles.homeItem}}
-					label={<span style={styles.menuText}>{Constants.PAGES.HOME.TITLE}</span>}
-					component={NavLink}
-					to={Constants.PAGES.HOME.ROUTE}/>
-				<Tab
-					style={styles.menuItem}
-					label={Constants.PAGES.CODING.TITLE}
-					component={NavLink}
-					to={Constants.PAGES.CODING.ROUTE}/>
-				<Tab
-					style={styles.menuItem}
-					label={Constants.PAGES.MUSIC.TITLE}
-					component={NavLink}
-					to={Constants.PAGES.MUSIC.ROUTE}/>
-				<Tab
-					style={styles.menuItem}
-					label={Constants.PAGES.PICTURE.TITLE}
-					component={NavLink}
-					to={Constants.PAGES.PICTURE.ROUTE}/>
-			</Toolbar>
+			<AppBar position="static" style={styles.menu}>
+				<Toolbar>
+					<IconButton color="inherit" aria-label="Menu" onClick={() => this.openMenu(!this.state.menuOpen)}>
+						<Menu
+							style={styles.menu}
+							open={this.state.menuOpen}
+							onClose={this.handleClose}>
+							{menuHeaders}
+						</Menu>
+						<MenuIcon/>
+					</IconButton>
+					{topHeaders}
+				</Toolbar>
+			</AppBar>
 		);
 	}
-
 }
 
 function createStyles() {
 	return {
 		menu: {
 			width: "100%",
-			minHeight: 0,
 			position: 'fixed',
 			top: 0,
-			zIndex: 100,
-			background: 'linear-gradient(to top right, #07000000, #00008B)',
-			opacity: 1
+			background: 'linear-gradient(to top right, #07000000, #00008B)'
 		},
 		menuItem: {
-			maxWidth: 'none',
+			minWidth: 200,
 			paddingLeft: 20,
 			paddingRight: 20,
 			color: 'white',
