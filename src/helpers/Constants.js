72c0f9a42265b99d95590034c704e00f62306457
Wei Tung Chen <lleeoochen@gmail.com>
2018-08-19 18:51:02 -0700

    Added and restructured image assets

--- src/helpers/Constants.js
+++ src/helpers/Constants.js
@@ -1,14 +1,21 @@
 export default class Constants {
 	static FONT = 'Comic Sans MS';
 	static PAGES = {
-		HOME:    { ROUTE: '/home',    TITLE: 'Wei Tung Chen' },
-		CODING:  { ROUTE: '/coding',  TITLE: 'Coding Projects' },
-		MUSIC:   { ROUTE: '/music',   TITLE: 'Music Recordings' },
-		PICTURE: { ROUTE: '/picture', TITLE: 'Photography' },
+		HOME:    { ROUTE: '/home',    TITLE: 'Wei Tung Chen'      },
+		CODING:  { ROUTE: '/coding',  TITLE: 'Coding Projects'    },
+		MUSIC:   { ROUTE: '/music',   TITLE: 'Music Recordings'   },
+		PICTURE: { ROUTE: '/picture', TITLE: 'Photography'        },
 		VIDEO:   { ROUTE: '/video',   TITLE: 'Educational Videos' }
 	};
 	static IMAGES = {
-		BACKGROUND: '/assets/background.png',
-		WHALE: '/assets/whale.png'
+		BACKGROUND:    '/assets/images/background.png',
+		WHALE:         '/assets/images/whale.png',
+		CHESS:         '/assets/images/web-chess.png',
+		CHROME_EDITOR: '/assets/images/chrome-editor.png',
+		GOLD_DIGGER:   '/assets/images/gold-digger.png',
+		DISK_GALAXY:   'https://github.com/lleeoochen/Disk-Galaxy/blob/master/Assets/Game_Screenshot.png?raw=true'
 	};
+	static APPS = {
+		NOTEBOOK_APP:  '/assets/applications/android_notebook_app.apk'
+	}
 }
